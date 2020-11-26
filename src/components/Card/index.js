3c4067a869c00076f793455a0e01351ee26ba775
rogtom <tomasz.rogala1505@gmail.com>
2020-11-26 14:29:16 +0100

    condition for adding to favorite

---
+++ src/components/Card/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Card, ListGroup } from 'react-bootstrap';
+import Location from '../Locations';
+import { AuthUserContext } from '../Session';
+import * as ROUTES from '../../constants/routes';
+import { Link } from 'react-router-dom';
+
+const FilmCard = ({title, picture, locations}) => {
+
+  return (
+    <Card className="Card" style={{ width: '18rem' } }>
+      <Card.Img variant="top" src={picture} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        {locations.map(location => <Location key={location.id} name={location.display_name} picture={location.icon}/>)}
+        {/*<Button variant="primary">Add to favorite</Button>*/}
+
+        <AuthUserContext.Consumer>
+          {authUser =>
+            authUser ? <Button variant="primary">Add to favorite</Button> : <Button variant="primary"><Link to={ROUTES.SIGN_IN}>Sign In</Link></Button>
+          }
+        </AuthUserContext.Consumer>
+      </Card.Body>
+    </Card>
+  )
+
+}
+export default FilmCard
