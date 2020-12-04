172f7f027cbc73815cce4da6a4822820d5774c13
rogtom <tomasz.rogala1505@gmail.com>
2020-12-04 13:10:23 +0100

    cleaning code style fix

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 const SignOutButton = ({ firebase }) => (
 
 
-    <Button variant="outline-secondary" className="text-white" size="sm" onClick={firebase.doSignOut}>Sign Out</Button>
+    <Button variant="outline-secondary" className="text-white" size="sm" onClick={firebase.doSignOut.then(this.props.history.push(ROUTES.HOME))}>Sign Out</Button>
 
 
 
